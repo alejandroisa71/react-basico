9bde733cb581bad6bdce8193c710eab1448d1902
Alejandro Isa <alejandroisa71@gmail.com>
2021-02-02 08:46:19 -0300

    repaso react basico

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import Carrito from './components/Carrito';
+import Footter from './components/Footer';
+import Header from './components/Header';
+import Producto from './components/Producto';
+
+
 
 function App() {
+
+
+// Crear listado de productos
+const[productos,guardarProductos]=useState([
+  { id:1, nombre:'Camisa ReactJS', precio:50 },
+  { id:2, nombre:'Camisa Node.js', precio:40 },
+  { id:3, nombre:'Camisa VueJS', precio:30 },
+  { id:4, nombre:'Camisa Angular', precio:20 },
+]);
+
+//Satate para un carrito de compras
+const [carrito,agregarProducto] = useState([]);
+
+//Obtener la fecha
+const fecha = new Date().getFullYear();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header
+        titulo='Tienda Virtual'
+      />
+      
+      <h1>Lista de Productos</h1>
+      {productos.map(producto=>(
+        <Producto
+          key={producto.id}
+          producto={producto}
+          productos={productos}
+          carrito={carrito}
+          agregarProducto={agregarProducto}
+        />
+      ))}
+
+      <Carrito
+        carrito={carrito}
+        agregarProducto={agregarProducto}
+      />
+
+      <Footter
+      fecha={fecha}
+      />
+    </>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footter = ({fecha}) => (
+        <footer>
+            <p>Todos los derechos reservados &copy; {fecha}</p>
+        </footer>
+);
+
+ 
+export default Footter;
